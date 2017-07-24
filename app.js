f0a018c2a24275be965be1e4d0854c586c503dc5
Panagiotis Zisis <takis.zisis@gmail.com>
2017-07-24 19:09:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const sassMiddleware = require('node-sass-middleware');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// middleware
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  outputStyle: 'compressed'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routing
+const index = require('./routes/index');
+app.use('/', index);
+
+io.on('connection', socket => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', data => {
+    console.log(data);
+  });
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('app is listening at port 3000...');
+});
